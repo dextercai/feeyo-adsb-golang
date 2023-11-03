067f4d86a4666310e52be0833c680b2f8a443e40
DexterCai <caiwenzhe2021@gmail.com>
2023-11-03 16:32:10 +0800

    迁移主程序代码启动位置

--- cmd/adsb/adsb.go
+++ cmd/adsb/adsb.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"dextercai.com/feeyo-adsb-golang/conf"
-	"fmt"
+	"dextercai.com/feeyo-adsb-golang/log"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	conf.InitConfig()
+	currentConfig := conf.ReadConfig()
+	log.InitLog(currentConfig.LogLevel, currentConfig.LogPath, currentConfig.LogFile,
+		currentConfig.LogRotationTime, currentConfig.LogMaxAge, currentConfig.LogRotationSize, currentConfig.LogRotationCount)
+	log.Logger.Debugf("配置读取: %#v", currentConfig)
 
-	fmt.Printf("%#v", conf.ReadConfig())
+	log.Logger.Warnf("将使用UUID: %s", currentConfig.FeeyoUUID)
+
+	//wg := sync.WaitGroup{}
+	//wg.Add(1)
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Kill, os.Interrupt)
+
+	sig := <-quit
+	log.Logger.WithField("scope", "process").Infof("收到信号: %s", sig.String())
+
+	//grpcServer.GracefulStop()
+	//wg.Wait()
+	log.Logger.WithField("scope", "process").Info("按预期关闭")
 }
